Enhance streamlines computation / correct some minor bugs

core.py
@@ -6499,9 +6499,9 @@ class VectorField(Field):
             if kind == 'quiver' or kind is None:
                 if 'C' in plotargs.keys():
                     C = plotargs.pop('C')
-                    if not (C == 0 or C is None):
-                        cb = plt.colorbar()
-                        cb.set_label("Magnitude " + unit_values.strUnit())
+                else:
+                    cb = plt.colorbar()
+                    cb.set_label("Magnitude " + unit_values.strUnit())
                 legendarrow = round(np.max([Vx.max(), Vy.max()]))
                 plt.quiverkey(displ, 1.075, 1.075, legendarrow,
                               "$" + str(legendarrow)
@@ -7778,9 +7778,10 @@ class TemporalFields(Fields, Field):
                 self.update()
 
             def update(self):
-                self.obj._update_sf(self.ind, self.fig, self.ax,
-                                    self.displ, self.ttl, self.comp,
-                                    self.compo, self.plotargs)
+                self.obj._update_sf(num=self.ind, fig=self.fig, ax=self.ax,
+                                    displ=self.displ, ttl=self.ttl,
+                                    comp=self.comp,
+                                    compo=self.compo, plotargs=self.plotargs)
                 if self.suppl_display is not None:
                     self.suppl_display(self.ind)
                 plt.draw()
@@ -8111,7 +8112,8 @@ class TemporalVectorFields(TemporalFields):
     def magnitude_as_sf(self):
         values = TemporalScalarFields()
         for i, field in enumerate(self.fields):
-            values.add_field(field.magnitude_as_sf)
+            values.add_field(field.magnitude_as_sf, time=self.times[i],
+                             unit_times=self.unit_times)
         return values
 
     @property
@@ -8126,7 +8128,8 @@ class TemporalVectorFields(TemporalFields):
     def theta_as_sf(self):
         values = TemporalScalarFields()
         for i, field in enumerate(self.fields):
-            values.add_field(field.theta_as_sf)
+            values.add_field(field.theta_as_sf, time=self.times[i],
+                             unit_times=self.unit_times)
         return values
 
     @property

field_treatment/field_treatment.py
@@ -711,7 +711,7 @@ def get_streamlines_fast(vf, xy, delta=.25, interp='linear',
         return streams
 
 def get_streamlines(VF, xys, reverse=False, rel_err=1.e-3,
-                     max_steps=5000, resolution=10.):
+                     max_steps=1000, resolution=0.1):
     """
     Return the lagrangien displacement of a set of particules initialy at the
     positions xys.
@@ -726,13 +726,15 @@ def get_streamlines(VF, xys, reverse=False, rel_err=1.e-3,
     rel_err : number
         relative maximum error for rk4 algorithm
     max_steps : integer
-        Maximum number of steps (default = 100).
+        Maximum number of steps (default = 1000).
     resolution : number,
-        resolution of the resulting streamline (do not impact accuracy)
+        resolution of the resulting streamline (do not impact accuracy).
     """
     # check
     if not isinstance(VF, VectorField):
         raise TypeError()
+    if np.any(VF.mask):
+        raise ValueError("Don't work on masked vector field, sorry...")
     if not isinstance(xys, ARRAYTYPES):
         raise TypeError()
     xys = np.array(xys)
@@ -768,6 +770,18 @@ def get_streamlines(VF, xys, reverse=False, rel_err=1.e-3,
         def fun(xy):
             return np.array([Vx_tor(*xy)[0][0],
                              Vy_tor(*xy)[0][0]])
+    # rkf45 algo for interpolation
+    def rkf45_interp(xy_init, rk_dt):
+        k1 = fun(xy_init)*rk_dt
+        k2 = fun(xy_init + k1/4.)*rk_dt
+        k3 = fun(xy_init + 3./32.*k1 + 9./32.*k2)*rk_dt
+        k4 = fun(xy_init + 1932./2197.*k1 - 7200./2197.*k2
+                 + 7296./2197.*k3)*rk_dt
+        k5 = fun(xy_init + 439./216.*k1 - 8*k2 + 3680./513.*k3
+                 - 845./4104.*k4)*rk_dt
+        new_xy = (xy_init + 25./216.*k1 + 1408./2565.*k3
+                  + 2197./4104.*k4 - 1./5.*k5)
+        return new_xy
     # rkf45 algo
     def rkf45(xy_init, t, rk_dt):
         k1 = fun(xy_init)*rk_dt
@@ -793,7 +807,7 @@ def get_streamlines(VF, xys, reverse=False, rel_err=1.e-3,
         if err == 0:
             new_rk_dt = 2*rk_dt
         else:
-            new_rk_dt = ((rel_err*rk_dt)/(2.*err))**.25/resolution
+            new_rk_dt = ((rel_err*rk_dt)/(2.*err))**.25
         t += rk_dt
         return new_xy, t, new_rk_dt
     # Loop on given points
@@ -813,6 +827,8 @@ def get_streamlines(VF, xys, reverse=False, rel_err=1.e-3,
         while True:
             new_xy = new_xys[-1]
             # stoping test
+            if np.any(np.isnan(new_xy)):
+                break
             if new_xy[0] < axe_x[0] or new_xy[0] > axe_x[-1]:
                 new_xys = new_xys[:-1]
                 break
@@ -821,11 +837,18 @@ def get_streamlines(VF, xys, reverse=False, rel_err=1.e-3,
                 break
             if nmb_it > max_steps:
                 break
-            if nmb_it != 1 \
-                    and np.linalg.norm(new_xys[-1] - new_xys[-2]) < dxy/1000.:
-                break
             # rk adaptative step
-            new_xy, t, rk_dt = rkf45(new_xy, t, rk_dt)
+            new_xy, t, new_rk_dt = rkf45(new_xy, t, rk_dt)
+            # if new_xy too big, use interpolation
+            dist_m1 = np.linalg.norm(new_xy - new_xys[-1])
+            if  dist_m1 > 2*dxy*resolution:
+                xy_init = new_xys[-1]
+                nmb_interp = int(dist_m1/(dxy*resolution))
+                for i in np.arange(0, nmb_interp - 1):
+                    tmp_xy = rkf45_interp(xy_init, rk_dt*(i + 1)/nmb_interp)
+                    new_xys.append(tmp_xy)
+            #
+            rk_dt = new_rk_dt
             new_xys.append(new_xy)
             nmb_it += 1
         # storing

vortex_detection/vortex_detection.py
@@ -1247,11 +1247,13 @@ class CritPoints(object):
         # display the critical lines
         if (field is not None and len(self.sadd[indice].xy) != 0
                 and isinstance(self.sadd[indice], OrientedPoints)):
+            if 'color' not in lnkw.keys():
+                lnkw['color'] = colors[3]
             streams = self.sadd[indice]\
                 .get_streamlines_from_orientations(field,
                 reverse_direction=[True, False], interp='cubic')
             for stream in streams:
-                stream._display(kind='plot', color=colors[4], **lnkw)
+                stream._display(kind='plot', **lnkw)
         # loop on the points types
         for i, pt in enumerate(self.iter):
             if pt[indice] is None:
@@ -3165,7 +3167,8 @@ def get_NL_residual_vorticity(vectorfield, radius=None, ind=False, mask=None,
     # getting the vorticity-tensor component
     omega = (Eyx - Exy)/2.
     omega_abs = np.abs(omega)
-    sign_omega = omega/omega_abs
+    sign_omega = np.sign(omega)
+    sign_omega[sign_omega == 0] = 1
     filt = s < omega_abs
     # getting the residual vorticity
     res_vort = np.zeros(vectorfield.shape)
@@ -4181,42 +4184,56 @@ def get_residual_vorticity(vf, raw=False):
         If 'True', return an arrays,
         if 'False' (default), return a ScalarField object.
     """
-    if not isinstance(vf, VectorField):
-        raise TypeError()
-    # getting data
-    tmp_vf = vf.copy()
-    tmp_vf.crop_masked_border()
-    tmp_vf.fill(inplace=True, reduce_tri=True)
-    axe_x, axe_y = tmp_vf.axe_x, tmp_vf.axe_y
-    comp_x, comp_y = tmp_vf.comp_x, tmp_vf.comp_y
-    mask = tmp_vf.mask
-    dx = axe_x[1] - axe_x[0]
-    dy = axe_y[1] - axe_y[0]
-    # getting gradients
-    Exx, Exy = np.gradient(comp_x, dx, dy)
-    Eyx, Eyy = np.gradient(comp_y, dx, dy)
-    # getting principal rate of strain (s)
-    s = np.sqrt(4*Exx**2 + (Exy + Eyx)**2)/2.
-    # getting the vorticity-tensor component
-    omega = (Eyx - Exy)/2.
-    omega_abs = np.abs(omega)
-    sign_omega = omega/omega_abs
-    filt = s < omega_abs
-    # getting the residual vorticity
-    res_vort = np.zeros(tmp_vf.shape)
-    res_vort[filt] = sign_omega[filt]*(omega_abs[filt] - s[filt])
-    if raw:
-        return res_vort
+    if isinstance(vf, VectorField):
+        # getting data
+        tmp_vf = vf.copy()
+        tmp_vf.fill(inplace=True, reduce_tri=True)
+        axe_x, axe_y = tmp_vf.axe_x, tmp_vf.axe_y
+        comp_x, comp_y = tmp_vf.comp_x, tmp_vf.comp_y
+        mask = tmp_vf.mask
+        dx = axe_x[1] - axe_x[0]
+        dy = axe_y[1] - axe_y[0]
+        # getting gradients
+        Exx, Exy = np.gradient(comp_x, dx, dy)
+        Eyx, Eyy = np.gradient(comp_y, dx, dy)
+        # getting principal rate of strain (s)
+        s = np.sqrt(4*Exx**2 + (Exy + Eyx)**2)/2.
+        # getting the vorticity-tensor component
+        omega = (Eyx - Exy)/2.
+        omega_abs = np.abs(omega)
+        sign_omega = np.zeros(omega.shape, dtype=int)
+        sign_omega[omega_abs == 0] = 1.
+        sign_omega[omega_abs != 0] = omega[omega_abs != 0]/omega_abs[omega_abs != 0]
+        filt = s < omega_abs
+        # getting the residual vorticity
+        res_vort = np.zeros(tmp_vf.shape)
+        res_vort[filt] = sign_omega[filt]*(omega_abs[filt] - s[filt])
+        if raw:
+            return res_vort
+        else:
+            unit_x, unit_y = tmp_vf.unit_x, tmp_vf.unit_y
+            unit_values = vf.unit_values/vf.unit_x
+            res_vort *= unit_values.asNumber()
+            unit_values /= unit_values.asNumber()
+            vort_sf = ScalarField()
+            vort_sf.import_from_arrays(axe_x, axe_y, res_vort, mask=mask,
+                                       unit_x=unit_x, unit_y=unit_y,
+                                       unit_values=unit_values)
+            return vort_sf
+    elif isinstance(vf, TemporalVectorFields):
+        if raw:
+            tvf = []
+            for field in vf.fields:
+                tvf.append(get_residual_vorticity(field, raw=True))
+        else:
+            tvf = TemporalScalarFields()
+            for i, field in enumerate(vf.fields):
+                tvf.add_field(get_residual_vorticity(field, raw=False),
+                              time=vf.times[i], unit_times=vf.unit_times)
+        # return
+        return tvf
     else:
-        unit_x, unit_y = tmp_vf.unit_x, tmp_vf.unit_y
-        unit_values = vf.unit_values/vf.unit_x
-        res_vort *= unit_values.asNumber()
-        unit_values /= unit_values.asNumber()
-        vort_sf = ScalarField()
-        vort_sf.import_from_arrays(axe_x, axe_y, res_vort, mask=mask,
-                                   unit_x=unit_x, unit_y=unit_y,
-                                   unit_values=unit_values)
-        return vort_sf
+        raise TypeError()
 
 
 def get_shear_vorticity(vf, raw=False):