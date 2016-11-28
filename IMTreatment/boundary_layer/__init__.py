998a01a9b7ba53971e97fd3c8a71027fa7f17d44
muahah <gnieheh@gmail.com>
2016-11-28 18:17:32 +0100

    Add setup.py

---
+++ IMTreatment/boundary_layer/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 20:46:48 2014
+
+@author: muahah
+"""
+
+from .boundary_layer import get_bl_thickness, get_displ_thickness,\
+    get_momentum_thickness, get_shape_factor, BlasiusBL, WakeLaw, WallLaw,\
+    get_shear_stress, get_clauser_thickness, FalknerSkanBL, ThwaitesBL
--- setup.py
+++ setup.py
@@ -4,29 +4,40 @@ from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import shutil
 
- 
+
 # submodules names
 submodule_names = ['boundary_layer',
                    'field_treatment',
                    'file_operation',
+                   'force_directed_algorithm',
                    'pod',
                    'protocols',
-                   'tools',
                    'vortex_creation',
+                   'vortex_criterions',
                    'vortex_detection',
-                   'vortex_criterions'] 
+                   ]
 
 # set extensions
-extensions = [Extension("core", ["core.py"], include_dirs=(".",))]
+extensions = []
 for name in submodule_names:
     path = r"./{0}/{0}.py".format(name)
     ext = Extension(name, [path], include_dirs=('.',))
     extensions.append(ext)
+extensions.append(Extension('utils', ['units.py', 'codeinteraction.py',
+                                      'files.py',
+                                      'multithreading.py', 'plot.py',
+                                      'progresscounter.py', 'types.py']))
+extensions.append(Extension("core", ["core.py"] , include_dirs=(".",)))
 
 # Distutils
 setup(
+    name='IMTreatment',
+    description='Tools to analyze PIV data',
+    author='Gaby Launay',
+    author_email='gaby.launay@tutanota.com',
     cmdclass = {'build_ext':build_ext},
     ext_modules = cythonize(extensions),
+    # ext_modules = extensions,
 
 )
 
