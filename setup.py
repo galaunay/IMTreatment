59c85fb8a81e888ba9930d42b3e545e8f34004ac
muahah <gnieheh@gmail.com>
2016-12-13 19:27:05 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,47 +1,40 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
-from Cython.Distutils import build_ext
-import shutil
+# from Cython.Build import cythonize
+# from Cython.Distutils import build_ext
 
 
-# submodules names
-submodule_names = ['boundary_layer',
-                   'field_treatment',
-                   'file_operation',
-                   'force_directed_algorithm',
-                   'pod',
-                   'protocols',
-                   'vortex_creation',
-                   'vortex_criterions',
-                   'vortex_detection',
-                   ]
-
-# set extensions
-extensions = []
-for name in submodule_names:
-    path = r"./{0}/{0}.py".format(name)
-    ext = Extension(name, [path], include_dirs=('.',))
-    extensions.append(ext)
-extensions.append(Extension('utils', ['units.py', 'codeinteraction.py',
-                                      'files.py',
-                                      'multithreading.py', 'plot.py',
-                                      'progresscounter.py', 'types.py']))
-extensions.append(Extension("core", ["core.py"] , include_dirs=(".",)))
+# For cython (to update)
+# # submodules names
+# submodule_names = ['boundary_layer',
+#                    'field_treatment',
+#                    'file_operation',
+#                    'force_directed_algorithm',
+#                    'pod',
+#                    'protocols',
+#                    'vortex_creation',
+#                    'vortex_criterions',
+#                    'vortex_detection',
+#                    ]
+# # set extensions
+# extensions = []
+# for name in submodule_names:
+#     path = r"./IMTreatment/{0}/{0}.py".format(name)
+#     ext = Extension(name, [path], include_dirs=('.',))
+#     extensions.append(ext)
+# extensions.append(Extension('utils', ['units.py', 'codeinteraction.py',
+#                                       'files.py',
+#                                       'multithreading.py', 'plot.py',
+#                                       'progresscounter.py', 'types.py'],
+#                             include_dirs=(".",)))
+# extensions.append(Extension("core", ["core.py"], include_dirs=(".",)))
 
 # Distutils
 setup(
     name='IMTreatment',
+    version='0.1',
     description='Tools to analyze PIV data',
     author='Gaby Launay',
     author_email='gaby.launay@tutanota.com',
-    cmdclass = {'build_ext':build_ext},
-    ext_modules = cythonize(extensions),
-    # ext_modules = extensions,
-
+    packages=['IMTreatment'],
 )
-
-
-# move *.pyd to the good folder
-#for name in submodule_names:
-#    shutil.move('./{0}.so'.format(name), r"./{0}")
